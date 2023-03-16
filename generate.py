d6f521df9af87f67fe5ad2129bf66974d093741f
Eamonn O'Brien-Strain <eob@well.com>
2023-03-16 14:46:16 -0700

    Add command-line parsing

--- generate.py
+++ generate.py
@@ -1,6 +1,6 @@
-import sys
-from moviepy.editor import CompositeVideoClip, ImageClip, VideoFileClip, ColorClip
 from math import sqrt, ceil
+from argparse import ArgumentParser
+from moviepy.editor import CompositeVideoClip, ImageClip, VideoFileClip, ColorClip
 
 approxThumbDuration = 1
 minThumbHeight = 30
@@ -9,22 +9,30 @@ WIDTH = 0
 HEIGHT = 1
 
 
-def main(originalPath):
+def main(originalPath, generateGif, generateVideo):
+    print("Generating gestalt for %s" % originalPath)
+    if (generateVideo):
+        print("Will generating video")
+    if (generateGif):
+        print("Will generating GIF")
+
     original = VideoFileClip(originalPath, audio=False)
     fullHeight = original.size[HEIGHT]
     fullWidth = original.size[WIDTH]
     maxThumbsPerSide = fullWidth//minThumbHeight
     thumbsPerSide = ceil(sqrt(original.duration/approxThumbDuration))
-    thumbsPerSide = max(2,thumbsPerSide)
-    thumbsPerSide = min(maxThumbsPerSide,thumbsPerSide)
+    thumbsPerSide = max(2, thumbsPerSide)
+    thumbsPerSide = min(maxThumbsPerSide, thumbsPerSide)
     thumbCount = thumbsPerSide*thumbsPerSide
     extendeWidth = fullWidth*(thumbsPerSide+1)//thumbsPerSide
     thumbHeight = fullHeight//thumbsPerSide
     thumbWidth = fullWidth//thumbsPerSide
     thumbDuration = original.duration/thumbCount
+    speedPixelsPerSec = thumbWidth/thumbDuration
+    speedPixelsPerFrame = speedPixelsPerSec/original.fps
 
-    print("%dx%d grid of %fx%f %fs thumbnails" %
-          (thumbsPerSide, thumbsPerSide, thumbWidth, thumbHeight, thumbDuration))
+    print("%dx%d grid of %fx%f %fs thumbnails, moving at @ %f pixels/frame" %
+          (thumbsPerSide, thumbsPerSide, thumbWidth, thumbHeight, thumbDuration, speedPixelsPerFrame))
 
     def motion(i, j):
         forward = j % 2 == 0
@@ -42,8 +50,8 @@ def main(originalPath):
                 .set_position(motion(i, j)))
 
     thumbs = [thumb(j, i)
-             for j in range(thumbsPerSide)
-             for i in range(thumbsPerSide)]
+              for j in range(thumbsPerSide)
+              for i in range(thumbsPerSide)]
 
     def left(j):
         k = j*thumbsPerSide - 1
@@ -77,17 +85,29 @@ def main(originalPath):
     output = CompositeVideoClip(
         thumbs+lefts+rights+[leading, trailing], (extendeWidth, fullHeight))
 
-    # output.write_gif("gestalt-"+originalPath+".gif", program="ffmpeg")
-    output.write_videofile("gestalt-"+originalPath)
+    if generateGif:
+        output.write_gif("gestalt-"+originalPath+".gif", program="ffmpeg")
+    if generateVideo:
+        output.write_videofile("gestalt-"+originalPath)
 
 
 if __name__ == "__main__":
     import time
-    args = sys.argv[1:]
-    if len(args) != 1:
-        print(f"Expected one argument got {len(args)}, {args}")
-        exit(1)
-    else:
-        start_time = time.time()
-        main(args[0])
-        print("--- %d seconds ---" % (time.time() - start_time))
+
+    parser = ArgumentParser(
+        prog='generate.py',
+        description='Generates overview of video',
+        epilog='(c) Eamonn O\'Brien-Strain')
+    parser.add_argument('-i', '--input', metavar='something.mp4',
+                        required=True,
+                        help='input video file')
+    parser.add_argument('-g', '--gif', action='store_true',
+                        default=False,
+                        help='generate GIF')
+    parser.add_argument('-v', '--video', action='store_true',
+                        default=False,
+                        help='generate video file')
+    args = parser.parse_args()
+    start_time = time.time()
+    main(args.input, args.gif, args.video)
+    print("--- %d seconds ---" % (time.time() - start_time))
